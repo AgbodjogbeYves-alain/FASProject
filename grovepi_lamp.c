ff470f78df4045462d69326d47b3954cc3d876b2
Yves-alain Agbodjogbe <Yves-alain Agbodjogbe>
2016-12-30 15:05:33 +0100

    grovepi_lanp

---
+++ grovepi_lamp.c
@@ -0,0 +1,20 @@
+#include <Python.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h> /* close */
+#include <netdb.h> /* gethostbyname */
+
+
+int main(){
+	PyObject *module,*function,*argument,*retour;
+	Py_Initialize(); 
+	PySys_SetPath(".");
+	module = PyImport_ImportModule("cli");
+	fonction = PyObject_GetAttrString(module, "turn");
+	arguments = Py_BuildValue("(s)", "on");
+	retour = PyEval_CallObject(fonction, arguments);
+	Py_Finalize(); 
+	return 0;
+}
