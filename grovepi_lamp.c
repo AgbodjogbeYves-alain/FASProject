4aa5eaa606420ae194cd2b8c86a03476e2b91a6b
Yves-alain Agbodjogbe <Yves-alain Agbodjogbe>
2017-01-03 15:34:13 +0100

    ajout fonction changer_couleur

--- grovepi_lamp.c
+++ grovepi_lamp.c
@@ -4,7 +4,28 @@
 #include <netdb.h> /* gethostbyname */
 
 
+void change_color(int cmd,char[] ip){
+	char commande[256];
+	char set_on[256];
+	char ip_bulb[] = ip;
+	sprintf(on,"yee --ip=%s turn on",ip);
+	system(on);
+	if(cmd == 1){
+
+	}
+	else if(cmd==2){
+
+	}
+	else if(cmd==3){
+
+	}
+	else if (cmd==4){
+
+	}
+}
+
 int main(){
+
 	/*PyObject *module,*fonction,*arguments,*retour;
 	Py_Initialize(); 
 	PySys_SetPath(".");
@@ -13,6 +34,7 @@ int main(){
 	arguments = Py_BuildValue("(s)", "on");
 	PyEval_CallObject(fonction, arguments);
 	Py_Finalize();*/
-	system("yee --ip=192.168.0.20 turn on");
+	//system("yee --ip=192.168.0.20 turn on");
+	change_color;
 	return 0;
 }
