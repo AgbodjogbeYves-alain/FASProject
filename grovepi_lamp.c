c53f17e3a7c2ec46a4b0caeb78004779381c5d12
AgbodjogbeYves-alain <agboniel@yahoo.fr>
2017-01-09 19:16:51 +0100

    modif main pour avoir le nombre de personnes et les decibels engendrés

--- grovepi_lamp.c
+++ grovepi_lamp.c
@@ -39,15 +39,16 @@ int main(){
 	int volume_actuel = 0;
 	int volumepers = 50;
 	int increment = 60;
+	int nbpers=0;
 	char ip[256];
 	int db;
 	printf("Entrez l'ip de la lampe");
 	scanf("%s",ip);
 	while(true){
 		printf("Entrez le volume sonore ambiant"); // a remplacer pr les valeurs du capteur sonore ou du nombre de personne * 50
-		scanf("%d",db);
+		scanf("%d",&db);
 		print("Entrez le nombre de personne dans la salle"); //a remplacer par les valeurs du capteur ultason
-		scanf("%d",nbpers);
+		scanf("%d",&nbpers);
 		
 		if (db>volume_actuel && db-volume_actuel>50){
 			while (db-volume_actuel > 50 ){
