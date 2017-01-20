bdb68327620d0078fdc1aa6de072d978ee88d06c
AgbodjogbeYves-alain <agboniel@yahoo.fr>
2017-01-20 16:37:40 +0100

    modif main

--- test.c
+++ test.c
@@ -7,7 +7,7 @@
 
 int main(){
 	system("export DISPLAY=:0");
-	system("eog --fullscreen carre_rouge.1240723866.thumbnail.png &");
-	system("eog --fullscrenn invisible.png &");
+	system("eog --fullscreen --new-instance carre_rouge.1240723866.thumbnail.png &");
+	system("eog --fullscrenn --new-instance invisible.png &");
 	return 0;
 }
