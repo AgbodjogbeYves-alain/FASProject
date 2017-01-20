6897e7845649f50695d2ab52964754f21695caf3
AgbodjogbeYves-alain <agboniel@yahoo.fr>
2017-01-20 13:13:01 +0100

    affichage image

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> /* close */
+#include <netdb.h> /* gethostbyname */
+
+
+
+int main(){
+
+	system("eog --fullscreen carre_rouge.1240723866.thumbnail.png");
+	sleep(2);
+	system("eog --fullscrenn invisible.png");
+}
